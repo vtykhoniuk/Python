14fed0c851e0c4e17fdb04c10c3159b0cdedb271
Volodymyr Tykhoniuk <volodymyr.tykhoniuk@gmail.com>
2016-09-27 18:05:32 +0300

    PEP-8 style fix

--- 2sum/test_2sum.py
+++ 2sum/test_2sum.py
@@ -2,6 +2,7 @@ import unittest
 
 from twosum import twosum, twosumv2
 
+
 class Test2Sum(unittest.TestCase):
 
     def test_2sum_raises_exception(self):
@@ -12,19 +13,16 @@ class Test2Sum(unittest.TestCase):
             twosum([], "a")
 
     def test_2sum_trivial(self):
-        self.assertListEqual([(1, 3)], twosum([1,2,3], 4));
-        self.assertListEqual([(1, 2)], twosum([1,2,3], 3));
-        self.assertListEqual([(1, 3), (2,2)], twosum([1,3,2,2], 4));
-        self.assertListEqual([(1, 2)], twosum([1,2,3,1], 3));
-        self.assertListEqual([(-1, 11), (1, 9), (2, 8), (3, 7), (4, 6), (5, 5)], sorted(twosum([1,9,2,8,3,7,4,6,5,5,13,14,11,13,-1], 10)));
+        self.assertListEqual([(1, 3)], twosum([1, 2, 3], 4))
+        self.assertListEqual([(1, 2)], twosum([1, 2, 3], 3))
+        self.assertListEqual([(1, 3), (2, 2)], twosum([1, 3, 2, 2], 4))
+        self.assertListEqual([(1, 2)], twosum([1, 2, 3, 1], 3))
+        self.assertListEqual([(-1, 11), (1, 9), (2, 8), (3, 7), (4, 6), (5, 5)], sorted(twosum([1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 13, 14, 11, 13, -1], 10)))
 
     def test_2sum_on_same_elements(self):
-        self.assertListEqual([(2, 2), (2, 2)], twosum([2,2,2,2], 4))
-        self.assertListEqual([(2, 2), (2, 2)], twosum([2,2,2,2,4], 4))
-        self.assertListEqual([(0, 4), (2, 2), (2, 2)], sorted(twosum([2,2,0,2,2,4], 4)))
-
-    def test_2sum_on_empty_set(self):
-        self.assertListEqual([], twosum([], 4))
+        self.assertListEqual([(2, 2), (2, 2)], twosum([2, 2, 2, 2], 4))
+        self.assertListEqual([(2, 2), (2, 2)], twosum([2, 2, 2, 2, 4], 4))
+        self.assertListEqual([(0, 4), (2, 2), (2, 2)], sorted(twosum([2, 2, 0, 2, 2, 4], 4)))
 
     def test_2sum_on_empty_set(self):
         self.assertListEqual([], twosum([], 4))
@@ -40,19 +38,16 @@ class Test2SumV2(unittest.TestCase):
             twosumv2([], "a")
 
     def test_2sum_trivial(self):
-        self.assertListEqual([(1, 3)], twosumv2([1,2,3], 4));
-        self.assertListEqual([(1, 2)], twosumv2([1,2,3], 3));
-        self.assertListEqual([(1, 3), (2,2)], twosumv2([1,3,2,2], 4));
-        self.assertListEqual([(1, 2)], twosumv2([1,2,3,1], 3));
-        self.assertListEqual([(-1, 11), (1, 9), (2, 8), (3, 7), (4, 6), (5, 5)], sorted(twosumv2([1,9,2,8,3,7,4,6,5,5,13,14,11,13,-1], 10)));
+        self.assertListEqual([(1, 3)], twosumv2([1, 2, 3], 4))
+        self.assertListEqual([(1, 2)], twosumv2([1, 2, 3], 3))
+        self.assertListEqual([(1, 3), (2, 2)], twosumv2([1, 3, 2, 2], 4))
+        self.assertListEqual([(1, 2)], twosumv2([1, 2, 3, 1], 3))
+        self.assertListEqual([(-1, 11), (1, 9), (2, 8), (3, 7), (4, 6), (5, 5)], sorted(twosumv2([1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 13, 14, 11, 13, -1], 10)))
 
     def test_2sum_on_same_elements(self):
-        self.assertListEqual([(2, 2), (2, 2)], twosumv2([2,2,2,2], 4))
-        self.assertListEqual([(2, 2), (2, 2)], twosumv2([2,2,2,2,4], 4))
-        self.assertListEqual([(0, 4), (2, 2), (2, 2)], sorted(twosumv2([2,2,0,2,2,4], 4)))
-
-    def test_2sum_on_empty_set(self):
-        self.assertListEqual([], twosumv2([], 4))
+        self.assertListEqual([(2, 2), (2, 2)], twosumv2([2, 2, 2, 2], 4))
+        self.assertListEqual([(2, 2), (2, 2)], twosumv2([2, 2, 2, 2, 4], 4))
+        self.assertListEqual([(0, 4), (2, 2), (2, 2)], sorted(twosumv2([2, 2, 0, 2, 2, 4], 4)))
 
     def test_2sum_on_empty_set(self):
         self.assertListEqual([], twosumv2([], 4))
