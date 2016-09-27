4e9a40530002ae5e0e1e49612782539b3d04b826
Volodymyr Tykhoniuk <volodymyr.tykhoniuk@gmail.com>
2016-09-27 18:06:33 +0300

    PEP-8 style fix

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,19 +1,23 @@
 from collections import defaultdict
 
+
 def isanagram(s1, s2):
     if not isinstance(s1, str) or not isinstance(s2, str):
         raise ValueError()
 
     d1 = defaultdict(int)
     for c in s1:
-        if c == " ": continue
+        if c == " ":
+            continue
         d1[c.lower()] += 1
 
     for c in s2:
-        if c == " ": continue
+        if c == " ":
+            continue
         c = c.lower()
-        if c not in d1: return False
-          
+        if c not in d1:
+            return False
+
         if d1[c] == 1:
             del(d1[c])
         else:
