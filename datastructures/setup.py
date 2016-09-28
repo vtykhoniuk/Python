d8a65009fbe6972e3659b92b77d77a829339d1ec
Volodymyr Tykhoniuk <volodymyr.tykhoniuk@gmail.com>
2016-09-28 14:23:56 +0300

    To be able to install package through the pip

---
+++ datastructures/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='datastructures',
+      version='0.1',
+      description='Popular computer science datastructures',
+      url='https://github.com/vtykhoniuk/Python/tree/master/datastructures',
+      author='Volodymyr Tykhoniuk',
+      author_email='volodymyr.tykhoniuk@gmail.com',
+      license='MIT',
+      packages=['datastructures'],
+      zip_safe=False)
